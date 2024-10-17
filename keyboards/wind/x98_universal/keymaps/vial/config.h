8858be78a31d63bee8c54642ab4c625f6ce349dd
clownfish-og <eriknimmo@gmail.com>
2024-10-17 13:48:27 -0400

    x

---
+++ keyboards/wind/x98_universal/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x6A, 0x4A, 0x60, 0xBA, 0x6B, 0x0E, 0xBB, 0x3D}
+/*
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+*/
