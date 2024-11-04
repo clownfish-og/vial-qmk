cb479c1b07f80fc23b34d8df06f29720132779ac
clownfish-og <eriknimmo@gmail.com>
2024-11-04 11:19:13 -0500

    added macros

--- keyboards/wind/x98/keymaps/vial/keymap.c
+++ keyboards/wind/x98/keymaps/vial/keymap.c
@@ -1,11 +1,73 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include QMK_KEYBOARD_H
-#include <quantum.h>
+
+enum custom_keycodes {
+    CAPGEN = QK_KB_0,
+    CHROME,
+    EXTEND
+};
+
+#ifdef RGB_MATRIX_ENABLE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case RGB_TOG:
+            if (record->event.pressed) {
+                switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL: {
+                        rgb_matrix_set_flags(LED_FLAG_NONE);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                    } break;
+                    default: {
+                        rgb_matrix_set_flags(LED_FLAG_ALL);
+                    } break;
+                }
+            }
+            if (!rgb_matrix_is_enabled()) {
+                rgb_matrix_set_flags(LED_FLAG_ALL);
+                rgb_matrix_enable();
+            }
+            return false;
+        case CAPGEN:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL("acvvvvv"));
+            }
+            return false;
+        case CHROME:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LALT(" ") SS_DELAY(200) ">chrome.exe" SS_DELAY(200) SS_TAP(X_ENT));
+            }
+            return false;
+        case EXTEND:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("P") SS_DELAY(500) SS_TAP(X_DOWN) SS_DELAY(100) SS_TAP(X_DOWN) SS_DELAY(200) SS_TAP(X_ENT) SS_DELAY(400) SS_TAP(X_ESC) );
+            }
+            return false;
+    }
+    return true;
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+            if (host_keyboard_led_state().caps_lock && !host_keyboard_led_state().num_lock) {
+                rgb_matrix_set_color_all(RGB_PURPLE);
+            }
+            else if (!host_keyboard_led_state().num_lock) {
+                rgb_matrix_set_color_all(RGB_WHITE);
+            }
+            else if (host_keyboard_led_state().caps_lock) {
+                rgb_matrix_set_color_all(RGB_BLUE);
+            }
+            else if (!rgb_matrix_get_flags()){
+                rgb_matrix_set_color_all(RGB_BLACK);
+            }
+        return true;
+}
+  #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-    KC_ESC,           KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,            MO(1),   KC_HOME, KC_END,  KC_MUTE,  QK_MACRO_0,
+    KC_ESC,           KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,            MO(1),   KC_HOME, KC_END,  KC_MUTE,  CHROME,
 	KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSLS, KC_DEL,  KC_NUM,  KC_PSLS, KC_PAST,  KC_PMNS,
 	KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,           KC_PGUP, KC_P7,   KC_P8,   KC_P9,    KC_PPLS,
 	KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,            KC_PGDN, KC_P4,   KC_P5,   KC_P6,
@@ -14,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 	[1] = LAYOUT(
-	_______,          KC_BRID, KC_BRIU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______,
+	EXTEND,           KC_BRID, KC_BRIU, _______, _______, _______, _______, _______, _______, _______, _______, _______, CAPGEN,           _______, _______, _______, _______, _______,
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, RGB_SAI, RGB_VAI, RGB_SPI, RGB_MOD,
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______, RGB_HUD, _______, RGB_HUI,
@@ -40,46 +102,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, _______, _______, _______,                                                       _______, _______, _______, _______, _______,          _______,          _______
     ),
 };
-
-
-#ifdef RGB_MATRIX_ENABLE
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case RGB_TOG:
-            if (record->event.pressed) {
-                switch (rgb_matrix_get_flags()) {
-                    case LED_FLAG_ALL: {
-                        rgb_matrix_set_flags(LED_FLAG_NONE);
-                        rgb_matrix_set_color_all(0, 0, 0);
-                    } break;
-                    default: {
-                        rgb_matrix_set_flags(LED_FLAG_ALL);
-                    } break;
-                }
-            }
-            if (!rgb_matrix_is_enabled()) {
-                rgb_matrix_set_flags(LED_FLAG_ALL);
-                rgb_matrix_enable();
-            }
-            return false;
-    }
-    return true;
-}
-
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-            if (host_keyboard_led_state().caps_lock && !host_keyboard_led_state().num_lock) {
-                rgb_matrix_set_color_all(RGB_PURPLE);
-            }
-            else if (!host_keyboard_led_state().num_lock) {
-                rgb_matrix_set_color_all(RGB_WHITE);
-            }
-            else if (host_keyboard_led_state().caps_lock) {
-                rgb_matrix_set_color_all(RGB_BLUE);
-            }
-            else if (!rgb_matrix_get_flags()){
-                rgb_matrix_set_color_all(0, 0, 0);
-            }
-        return true;
-}
-  #endif
