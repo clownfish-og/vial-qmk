c83ea84e8f1424989ebc05f6dc46f3a29c0ac1e1
clownfish-og <eriknimmo@gmail.com>
2024-11-08 11:49:02 -0500

    lots of config edits, format vial.json

---
+++ keyboards/wind/x98/config.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#if defined(RGB_MATRIX_ENABLE)
+#define RGB_MATRIX_LED_COUNT 6
+#define WS2812_EXTERNAL_PULLUP
+#define WS2812_PWM_DRIVER PWMD3
+#define WS2812_PWM_CHANNEL 1
+#define WS2812_PWM_PAL_MODE 1
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM3
+#define WS2812_DMA_CHANNEL 3
+#define WS2812_PWM_TARGET_PERIOD 800000
+#endif
--- keyboards/wind/x98/keymaps/vial/keymap.c
+++ keyboards/wind/x98/keymaps/vial/keymap.c
@@ -8,27 +8,8 @@ enum custom_keycodes {
     EXTEND
 };
 
-#ifdef RGB_MATRIX_ENABLE
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case RGB_TOG:
-            if (record->event.pressed) {
-                switch (rgb_matrix_get_flags()) {
-                    case LED_FLAG_ALL: {
-                        rgb_matrix_set_flags(LED_FLAG_NONE);
-                        rgb_matrix_set_color_all(0, 0, 0);
-                    } break;
-                    default: {
-                        rgb_matrix_set_flags(LED_FLAG_ALL);
-                    } break;
-                }
-            }
-            if (!rgb_matrix_is_enabled()) {
-                rgb_matrix_set_flags(LED_FLAG_ALL);
-                rgb_matrix_enable();
-            }
-            return false;
         case CAPGEN:
             if (record->event.pressed) {
                 SEND_STRING(SS_LCTL("acvvvvv"));
@@ -47,23 +28,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-            if (host_keyboard_led_state().caps_lock && !host_keyboard_led_state().num_lock) {
-                rgb_matrix_set_color_all(RGB_PURPLE);
-            }
-            else if (!host_keyboard_led_state().num_lock) {
-                rgb_matrix_set_color_all(RGB_WHITE);
-            }
-            else if (host_keyboard_led_state().caps_lock) {
-                rgb_matrix_set_color_all(RGB_BLUE);
-            }
-            else if (!rgb_matrix_get_flags()){
-                rgb_matrix_set_color_all(RGB_BLACK);
-            }
-        return true;
-}
-  #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
