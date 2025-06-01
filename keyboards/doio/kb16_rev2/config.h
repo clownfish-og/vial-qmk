e27158df68a65d8eaac389b602cbb5b6147df949
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-06-01 18:09:41 -0400

    restructure

--- keyboards/doio/kb16_rev2/config.h
+++ keyboards/doio/kb16_rev2/config.h
@@ -19,3 +19,9 @@
 
 /* Use the custom font */
 #define OLED_FONT_H "./lib/glcdfont.c"
+#ifdef OLED_ENABLE
+    /* Mapping I2C2 for OLED */
+    #define I2C1_SCL_PIN B10
+    #define I2C1_SDA_PIN B11
+    #define I2C_DRIVER I2CD2
+#endif
